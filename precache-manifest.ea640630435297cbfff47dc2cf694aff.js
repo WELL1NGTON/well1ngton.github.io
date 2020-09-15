d53aae6826b09c1840ce8ac02847c661257e2430
Wellington Carlos Massola <wellingtonmassola@gmail.com>
2020-09-15 12:37:33 -0300

    Updates

---
+++ precache-manifest.ea640630435297cbfff47dc2cf694aff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902695c19af0a97c640e137f59958de1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c1e9280af9e3621a0df",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f20a8850a237fc0785e8",
+    "url": "/static/js/2.b0b83ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0b83ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1e9280af9e3621a0df",
+    "url": "/static/js/main.eaa90b7f.chunk.js"
+  },
+  {
+    "revision": "eed70188447fb6407168",
+    "url": "/static/js/runtime-main.19a03d09.js"
+  }
+]);
