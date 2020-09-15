9c4bb23e25e366c6024f6287e949ffeb0470b2a1
Wellington Carlos Massola <wellingtonmassola@gmail.com>
2020-09-15 12:19:30 -0300

    Commit Inicial - Projeto React Inicializado

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <a className="App-link" href="https://well1ngton.github.io/criador_de_cargas_a_partir_de_xmls/" target="_blank" rel="noopener noreferrer">
+            Montar Cargas Com XMLs (react-js)
+          </a>
+        </p>
+        {/* <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
